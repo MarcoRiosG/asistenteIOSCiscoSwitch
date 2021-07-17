bfd8b611c7351e4a7d8629748c3277f5306a2320
MarcoRios <a01378840@itesm.mx>
2021-07-16 20:22:46 -0500

    added another test

--- test_asistente.py
+++ test_asistente.py
@@ -30,3 +30,13 @@ def test_vlan_acceso(monkeypatch):
     expected_comands = "\ninterface vlan 1\nip address 192.168.0.10 255.255.255.0\nno shutdown \nexit \ndo copy run start \n"
 
     assert returned_comands == expected_comands
+
+def test_vlans(monkeypatch):
+
+    answers = iter(["", "r", "2", "x", "prueba", "s", "", "r", "2", "3", "4", "5", "6", "X", "", "r", "T", "", "2,5", "t", "r", "1", "", "A", "", "a", "6"])
+    comandos = ""
+    monkeypatch.setattr('builtins.input', lambda _: next(answers))
+    returned_comands = vlans.vlan(comandos)
+    expected_comands = "\nvlan 2\nname prueba\nexit\ninterface f0/2\ninterface f0/3\nswitchport mode trunk\nswitchport trunk allowed vlan 2,5\ninterface f0/4\nswitchport mode trunk\nswitchport trunk native vlan 1\ninterface f0/5\nswitchport mode access\ninterface f0/6\nswitchport mode access\nswitchport access vlan 6\nexit\ndo copy run start \n"
+
+    assert returned_comands == expected_comands
